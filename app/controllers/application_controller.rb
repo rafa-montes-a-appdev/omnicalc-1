de09ddb9ee854a116dde5b3cc4d5b2b1ef6b758a
Rafael Montes <rmontes@chicagobooth.edu>
2023-04-10 00:01:19 +0000

    Square works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
 class ApplicationController < ActionController::Base
+ layout(false)
 
+ 
+  def blank_square_form
+    render({:template => "/calculation_templates/square_form.html.erb"})
+  end 
+
+
+  def calculate_square
+    @num = params.fetch("number").to_f
+    @square_of_num = @num ** 2
+    render({:template => "/calculation_templates/square_results.html.erb"})
+  end 
+
+
+  def square_root_form
+
+    render()
+  end 
+
+
+  def payment_form
+
+    render()
+  end 
+
+
+  def random_form
+
+    render()
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+  get("/square/new", {:controller => "application", :action => "blank_square_form"})
+  get("/square/results", {:controller => "application", :action => "calculate_square"})
+
+  get("/square", {:controller => "application", :action => "square_form"})
+  get("/square_root", {:controller => "application", :action => "square_root_form"})
+  get("/payment",{:controller => "application",:action => "payment_form"})
+  get("/random",{:controller => "application", :action => "random_form"})
 
 end
