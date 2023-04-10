e158d644845d0a6afe79a78f8e1c00fa9365bd9d
Rafael Montes <rmontes@chicagobooth.edu>
2023-04-10 01:22:51 +0000

    Final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
- layout(false)
-
+ layout("wrapper.html.erb")
  
   def blank_square_form
     render({:template => "/calculation_templates/square_form.html.erb"})
@@ -8,26 +7,52 @@ class ApplicationController < ActionController::Base
 
 
   def calculate_square
-    @num = params.fetch("number").to_f
+    @num = params.fetch("sq_input").to_f
     @square_of_num = @num ** 2
     render({:template => "/calculation_templates/square_results.html.erb"})
   end 
 
 
-  def square_root_form
+  def blank_square_root_form
 
-    render()
+    render({:template => "/calculation_templates/square_root_form.html.erb"})
   end 
 
-
-  def payment_form
-
-    render()
+  def calculate_square_root
+    @sq_root_input = params.fetch("sq_root_input").to_f
+    @square_root_of_input = @sq_root_input ** (0.5)
+    render({:template => "/calculation_templates/square_root_results.html.erb"})
   end 
 
+  def blank_payment_form
 
-  def random_form
-
-    render()
+    render({:template => "/calculation_templates/payment_form.html.erb"})
   end 
+
+  def calculate_payment
+    @apr = params.fetch("user_apr").to_f/100.0
+    @apr_disp = "#{(100*@apr).round(4)}%"
+    @years = params.fetch("user_years").to_f
+    @months = @years*12
+    @principal = params.fetch("user_principal").to_f
+
+    @payment_num = @apr/12.0 * @principal
+    @payment_den = 1 - (1 + @apr/12.0)**(-@months)
+    @payment = (@payment_num/@payment_den).round(2)
+
+    render({:template => "/calculation_templates/payment_results.html.erb"})
+  end 
+
+  def blank_random_form
+
+    render({:template => "/calculation_templates/random_form.html.erb"})
+  end 
+
+  def calculate_random
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @random_number = rand(@lower..@upper)
+    render({:template => "/calculation_templates/random_results.html.erb"})
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   get("/square/new", {:controller => "application", :action => "blank_square_form"})
   get("/square/results", {:controller => "application", :action => "calculate_square"})
 
-  get("/square", {:controller => "application", :action => "square_form"})
-  get("/square_root", {:controller => "application", :action => "square_root_form"})
-  get("/payment",{:controller => "application",:action => "payment_form"})
-  get("/random",{:controller => "application", :action => "random_form"})
+  get("/square_root/new", {:controller => "application", :action => "blank_square_root_form"})
+  get("/square_root/results", {:controller => "application", :action => "calculate_square_root"})
+
+  get("/payment/new",{:controller => "application",:action => "blank_payment_form"})
+  get("/payment/results",{:controller => "application",:action => "calculate_payment"})
+  
+  get("/random/new",{:controller => "application", :action => "blank_random_form"})
+  get("/random/results",{:controller => "application", :action => "calculate_random"})
 
 end
